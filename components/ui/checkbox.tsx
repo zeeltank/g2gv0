def3629fc7b1a324f4fc48b698a85a87324c998e
Your Name <milabaldaniya29@gmail.com>
2026-06-24 11:10:40 +0530

    feat: implement employee directory module with reusable UI components and data management tables

--- components/ui/checkbox.tsx
+++ components/ui/checkbox.tsx
@@ -2,12 +2,35 @@ import * as React from 'react'
 import { Check } from 'lucide-react'
 import { cn } from '@/lib/utils'
 
-interface CheckboxProps extends React.InputHTMLAttributes<HTMLInputElement> {
+interface CheckboxProps extends Omit<React.InputHTMLAttributes<HTMLInputElement>, 'size'> {
   size?: 'sm' | 'default' | 'lg'
+  indeterminate?: boolean
 }
 
 const Checkbox = React.forwardRef<HTMLInputElement, CheckboxProps>(
-  ({ className, size = 'default', ...props }, ref) => {
+  ({ className, size = 'default', indeterminate, ...props }, ref) => {
+    const innerRef = React.useRef<HTMLInputElement>(null)
+
+    React.useEffect(() => {
+      if (innerRef.current) {
+        innerRef.current.indeterminate = indeterminate ?? false
+      }
+    }, [indeterminate])
+
+    const combinedRef = React.useCallback(
+      (node: HTMLInputElement) => {
+        if (typeof ref === 'function') ref(node)
+        else if (ref) (ref as React.MutableRefObject<HTMLInputElement>).current = node
+
+        // @ts-ignore
+        innerRef.current = node
+        if (node) {
+          node.indeterminate = indeterminate ?? false
+        }
+      },
+      [ref, indeterminate]
+    )
+
     const sizeClass = {
       sm: 'size-4',
       default: 'size-5',
@@ -23,7 +46,7 @@ const Checkbox = React.forwardRef<HTMLInputElement, CheckboxProps>(
     return (
       <div className="relative inline-flex">
         <input
-          ref={ref}
+          ref={combinedRef}
           type="checkbox"
           className={cn(
             'peer appearance-none cursor-pointer rounded border border-input bg-background transition-all focus-visible:outline-none focus-visible:ring-3 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 checked:border-primary checked:bg-primary dark:checked:border-primary dark:checked:bg-primary',
